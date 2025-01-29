9a80b1f6ded1d199b92dcb841abb4c72092fc5cc
voraingautam <vorain.gautam@edu.sait.ca>
2025-01-28 22:17:41 -0700

    first commmit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Vorain Gautam</p>
+            <p>
+                Link to repository <Link href="https://github.com/voraingautam/cprg306-assignments.git">https://github.com/voraingautam/cprg306-assignments.git</Link>
+            </p>
+        </div>
+    )
+}
